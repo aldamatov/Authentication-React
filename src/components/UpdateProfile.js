0a74ebb7039c55e0989f752a2e019c63e09dbf50
unknown <aldamatov@gmail.com>
2022-07-11 14:16:27 +1000

    Login

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,62 +1,71 @@
-import React, { useRef, useState } from "react"
+ import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useNavigate } from "react-router-dom"
 
-export default function Signup() {
+export default function UpdateProfile() {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  const { signup } = useAuth()
+  const { currentUser, updateEmail, updatePassword  } = useAuth()
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
+  const [message, setMessage] = useState("")
 
-  async function handleSubmit(e) {
+ function handleSubmit(e) {
     e.preventDefault()
 
     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
       return setError("Passwords do not match")
     }
 
-    try {
-      setError("")
-      setLoading(true)
-      await signup(emailRef.current.value, passwordRef.current.value)
-      navigate("/")
-    } catch {
-      setError("Failed to create an account")
+    const promises = [];
+    setLoading(true);
+    setError('')
+    if(emailRef.current.value !== currentUser.email){
+        promises.push(updateEmail(emailRef.current.value))
+    }
+    if(passwordRef.current.value){
+        promises.push(updatePassword(passwordRef.current.value))
     }
 
-    setLoading(false)
+    Promise.all(promises).then(() => {
+        navigate('/')
+        alert('Account updated')
+    }).catch(() => {
+        setError('Failed to update account')
+    }).finally(() => {
+        setLoading(false)
+    })
   }
 
   return (
     <>
       <Card>
         <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>  
+          <h2 className="text-center mb-4">Update Profile</h2>  
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              <Form.Control type="email" ref={emailRef} required defaultValue={currentUser.email} />
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
+              <Form.Control type="password" ref={passwordRef} placeholder="Leave blank to keep the same"/>
             </Form.Group>
             <Form.Group id="password-confirm">
               <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
+              <Form.Control type="password" ref={passwordConfirmRef} placeholder="Leave blank to keep the same" />
             </Form.Group>
             <Button disabled={loading} className="w-100 mt-4" type="submit">
-              Sign Up
+             Update
             </Button>
           </Form>
         </Card.Body>
       <div className="w-100 text-center mt-2 mb-3">
-        Already have an account? <Link to="/login">Log In</Link>
+       <Link to="/">Cancel</Link>
       </div>
       </Card>
     </>
